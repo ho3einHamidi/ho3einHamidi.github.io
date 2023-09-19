e281d3ade29bd502717846a222533722cd387979
ho3einHamidi <hosseinmhh@yahoo.com>
2023-09-19 19:21:00 +0330

    fix: login wrapper ui

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("scrollup");
   }
 });
+
+const getPosts = async  () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+  return posts.slice(0,4);
+}
+const createPost = (post) => {
+  console.log(post);
+  const newsWrapper = document.querySelector(".secondry-news")
+  const news = document.createElement("div")
+  news.classList.add("side-news");
+  const newsImage = document.createElement("img")
+  newsImage.setAttribute("src","")
+  const newsDescriptionSection = document.createElement("div")
+  newsDescriptionSection.classList.add("caption");
+  const newsTitle = document.createElement("p")
+  newsTitle.classList.add("title")
+  newsTitle.innerHTML = post.title
+  const newsDescription = document.createElement("p")
+  newsDescription.classList.add("description")
+  newsDescription.innerHTML = post.body
+
+  newsDescriptionSection.appendChild(newsDescription)
+  newsDescriptionSection.appendChild(newsTitle)
+  news.appendChild(newsImage)
+
+  news.appendChild(newsDescriptionSection)
+  newsWrapper.appendChild(news)
+}
+window.addEventListener("load",() => {
+   getPosts().then((data) => {
+    data.map((post) => createPost(post))
+  })
+})
